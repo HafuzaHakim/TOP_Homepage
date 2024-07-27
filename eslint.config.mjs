d64835ca721714b91d207dc35e21f99bd965ba90
HafuzaHakim <hafuzahakim@gmail.com>
2024-07-28 06:40:26 +0800

    Added CSS reset rules

--- eslint.config.mjs
+++
@@ -1,4 +0,0 @@
-import config from 'eslint-config-airbnb';
-import eslintConfigPrettier from 'eslint-config-prettier';
-
-export default [config, eslintConfigPrettier];
--- src/index.js
+++
@@ -1,1 +0,0 @@
-const straw = 'this is a string';
